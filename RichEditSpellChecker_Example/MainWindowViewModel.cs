b66bd2066db10999d94cabd220977b92a494726e
ajones <ajones@archetype-entertainment.com>
2022-07-28 09:27:37 -0500

    Added repro code

---
+++ RichEditSpellChecker_Example/MainWindowViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevExpress.Mvvm;
+using DevExpress.Xpf.Bars;
+
+namespace RichEditSpellChecker_Example
+{
+    public class MainWindowViewModel
+    {
+        public List<BarItem> ItemLinks { get; } = new()
+        {
+            new BarButtonItem() {Content = "Hello, World!"}
+        };
+    }
+}
